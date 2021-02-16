c7ad0672afc426ce0d098b075f0c77a0ef1643e5
asma20 <jbeli.asma20@gmail.com>
2021-02-16 11:29:37 +0100

    Afficher un text avec la date actuelle

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercice5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String nom;
+            String prenom;
+            DateTime date=DateTime.Today;
+
+            Console.WriteLine("quelle est votre nom : ");
+            nom= Console.ReadLine();
+            Console.WriteLine("quelle est votre prenom : ");
+            prenom = Console.ReadLine();
+          
+
+            Console.WriteLine(" Bonjour {0} {1}, nous sommes {2}, comment allez-vous ? ",nom,prenom, date.ToString("d"));
+
+        }
+    }
+}
